41e3ebfc688fb874bd6902aba3f3a621ce3fd588
Patrick Minda <patrick.minda@hotmail.com>
2024-12-24 09:43:09 +1100

    Frontend foundations.

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+    let data: any = {};
+
+    const VITE_SERVER_URL = "http://127.0.0.1:5000"; 
+    const fetchData = async () => {
+        const response = await fetch(VITE_SERVER_URL + '/api/data');
+        data = await response.json();
+    };
+
+    onMount(async() => {
+
+        await fetchData();
+    })
+</script>
+
+<h1>{data.message}</h1>
+<p>Status: {data.status}</p>
